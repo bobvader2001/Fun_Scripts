ed7351b7e49549767fa456f39ecca30f04682438
bobvader2001 <pelyma.matthew@icloud.com>
2018-07-16 22:46:06 +0100

    Added CounterPhish Script
    Some scammer took down his phishing page as a result of this script. Memes.

---
+++ CounterPhish.py
@@ -0,0 +1,16 @@
+import requests
+import random
+import string
+
+counter = 1
+while(True):
+    user = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase, k=random.randint(5,15)))
+    email = user + '@btinternet.com'
+    password = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + string.digits + '!@#$%^&*()_', k=random.randint(3,25)))
+    data = email + ' - ' + password
+    print(str(counter) + ': ' + data)
+    r = requests.post("http://A URL.com/face/btins/next.php", data={'u':email, 'p':password})
+    if "Access denied.  Your IP address is blacklisted." in r.text:
+        print(r.text)
+        exit(0)
+    counter += 1
